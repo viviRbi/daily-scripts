d0d53023b2214eba23e88fd7611463a86a8a59a6
Vy Le <50475727+viviRbi@users.noreply.github.com>
2025-04-21 09:35:52 +0700

    Update diary&gameFolder.sh

--- diary&gameFolder.sh
+++ diary&gameFolder.sh
@@ -23,7 +23,7 @@ speak_hi() {
   Add-Type -AssemblyName System.Speech;
   \$synth = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer;
   \$synth.Rate = -2; # negative = slower speech
-  \$message = 'Hello, V. Today is $current_date, and as the clock strikes $current_time, the world continues its endless dance, moving through time and space. Tell me, how does your heart feel today?';
+  \$message = 'Hello, V. Today is $current_date, and as the clock strikes $current_time, the world continues its endless dance, moving through time and space.';
   \$synth.Speak(\$message)
 "
 }
@@ -33,31 +33,33 @@ speak_thank_you() {
     Add-Type -AssemblyName System.Speech;
     \$synth = New-Object System.Speech.Synthesis.SpeechSynthesizer;
     \$synth.Rate = -2;
-    \$synth.Speak('Thank you for sharing your thoughts with me. In a world constantly moving, it''s the moments of reflection, like this one, that remind us of the romance woven into our experiences. Just as the stars shine quietly above, our lives unfold in a dance of fleeting moments, each one holding its own quiet beauty. Vicous Fate will become the star that shines brightly in a vast ocean. Even years after players finish the game, they''ll still remember the story and its characters — like echoes from a dream that lingers.')
+    \$synth.Speak('Thank you for sharing your thoughts with me. In a world constantly moving, it''s the moments of reflection, like this one, that remind us of the romance woven into our experiences. Just as the stars shine quietly above, our lives unfold in a dance of fleeting moments, each one holding its own quiet beauty.')
   "
 }
 
 
 # Start here
+open_target_folder
+
 speak_hi
 
-echo "How does your heart feel today?"
-read user_feeling
+#echo "How do you feel today?"
+#read user_feeling
 
-if [ -z "$user_feeling" ]; then
-  echo "No input detected."
-  speak_thank_you
-  open_target_folder
-  exit 0
-fi
-
-speak_thank_you
+#if [ -z "$user_feeling" ]; then
+#  echo "No input detected."
+#  speak_thank_you
+#  open_target_folder
+#  exit 0
+#fi
 
 diary_file="$HOME/Desktop/diary.md"
 
 
 # save to diary file
-echo -e "-------\n$(date "+%Y-%m-%d %I:%M %p")\n$user_feeling" >> "$diary_file"
+#echo -e "-------\n$(date "+%Y-%m-%d %I:%M %p")\n$user_feeling" >> "$diary_file"
 
 
-open_target_folder
+
+#speak_thank_you
+
